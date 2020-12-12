e8ff7329ef167d9a8c4bfdc8da5a168299bdffcc
mrun255dumbre <mrun255dumbre@gmail.com>
2020-12-12 13:04:33 +0530

    Refactored : code to to Add or Update Employee Payroll data to JSON Server

--- js/EmployeePayroll.js
+++ js/EmployeePayroll.js
@@ -43,12 +43,33 @@ const save = (event) => {
     event.stopPropagation();
     try{
         setEmployeePayrollObject();
-        createAndUpdateStorage();
-        resetForm();
-        window.location.replace(site_properties.home_page);
+        if(site_properties.use_local_storage.match("true")){
+            createAndUpdateStorage();
+            resetForm();
+            window.location.replace(site_properties.home_page);
+        } else {
+            createOrUpdateEmployeePayroll();
+        }
     } catch(exception){
         return;
     }
+}
+
+const createOrUpdateEmployeePayroll = () => {
+    let postURL = site_properties.server_url;
+    let methodCall = "POST";
+    if(isUpdate){
+        methodCall = "PUT";
+        postURL = postURL + employeePayrollObj.id.toString();
+    }
+    makeServiceCall(methodCall, postURL, true, employeePayrollObj)
+        .then(responseText => {
+            resetForm();
+            window.location.replace(site_properties.home_page);
+        })
+        .catch(error => {
+            throw error;
+        });
 }
 
 const setEmployeePayrollObject = () => {
--- js/site_properties.js
+++ js/site_properties.js
@@ -2,5 +2,5 @@ let site_properties = {
     use_local_storage: "false",
     home_page: "../pages/Home.html",
     add_emp_payroll_page: "../pages/EmployeePayroll.html",
-    server_url: "http://127.0.0.1:3000/employees"
+    server_url: "http://127.0.0.1:3000/employees/"
 };
