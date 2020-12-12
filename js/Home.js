5eb7d38542d2163bf1d40323018f2967792f7108
mrun255dumbre <mrun255dumbre@gmail.com>
2020-12-12 14:01:07 +0530

    Refactored : code to remove Employee from Payroll Service to JSON Server

--- js/Home.js
+++ js/Home.js
@@ -70,8 +70,22 @@ const remove = (node) => {
     const index = employeePayrollList.map(empData => empData.id)
                   .indexOf(employeePayrollData.id);
     employeePayrollList.splice(index, 1);
-    localStorage.setItem("EmployeePayrollList", JSON.stringify(employeePayrollList));
-    createInnerHtml();              
+    if(site_properties.use_local_storage.match("true")){
+        localStorage.setItem("EmployeePayrollList", JSON.stringify(employeePayrollList));
+        document.querySelector(".emp-count").textContent = employeePayrollList.length;
+        createInnerHtml();   
+    } else {
+        const deleteURL = site_properties.server_url + employeePayrollData.id.toString();
+        makeServiceCall("DELETE", deleteURL, false)
+            .then(responseText => {
+                document.querySelector(".emp-count").textContent = employeePayrollList.length;
+                createInnerHtml();
+            })
+            .catch(error => {
+                console.log("Delete Error Status: "+JSON.stringify(error));
+            });
+        
+    }               
 }
 
 const update = (node) => {
